f817deab074bfc86272dd7c78f6e684505d4ce23
mdubois6@UNIV-FCOMTE.FR <mdubois6@UNIV-FCOMTE.FR@31180c16-7d4d-425e-b24e-9feb305d5524>
2015-12-22 13:55:33 +0000

    selenium tests should use proxy now
    git-svn-id: http://m2gl.deptinfo-st.univ-fcomte.fr/svn/m2test6@10 31180c16-7d4d-425e-b24e-9feb305d5524

--- markattaks-tmp/src/test/java/TestBugWebpageTest.java
+++ markattaks-tmp/src/test/java/TestBugWebpageTest.java
@@ -1,9 +1,12 @@
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestBugWebpageTest {
     private String baseUrl;
+    private WebClient webClient;
 
     @Before
     public void setUp() throws Exception {
@@ -11,13 +14,24 @@ public class TestBugWebpageTest {
         if (baseUrl == null) {
             baseUrl = "http://php-facky.rhcloud.com/markattaks-tmp/website";
         }
+        String httpProxyHost = System.getenv("http.proxyHost");
+        if (httpProxyHost != null) {
+            int httpProxyPort = Integer.parseInt(System.getenv("http.proxyPort"));
+            webClient = new WebClient(BrowserVersion.getDefault(), httpProxyHost, httpProxyPort);
+        } else {
+            webClient = new WebClient(BrowserVersion.getDefault());
+        }
+    }
+
+    @After
+    public void tearDown() {
+        webClient.close();
     }
 
     @Test(expected = com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException.class)
     public void testGET() throws Exception {
-        try (WebClient webClient = new WebClient()) {
-            webClient.getPage(baseUrl + "/testbug.php");
-        }
+        webClient.getPage(baseUrl + "/testbug.php");
     }
+
 
 }
--- markattaks-tmp/src/test/java/TestWebpageTest.java
+++ markattaks-tmp/src/test/java/TestWebpageTest.java
@@ -1,12 +1,14 @@
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.WebClient;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestWebpageTest {
-
     private String baseUrl;
+    private WebClient webClient;
 
     @Before
     public void setUp() throws Exception {
@@ -14,16 +16,27 @@ public class TestWebpageTest {
         if (baseUrl == null) {
             baseUrl = "http://php-facky.rhcloud.com/markattaks-tmp/website";
         }
+        String httpProxyHost = System.getenv("http.proxyHost");
+        if (httpProxyHost != null) {
+            int httpProxyPort = Integer.parseInt(System.getenv("http.proxyPort"));
+            webClient = new WebClient(BrowserVersion.getDefault(), httpProxyHost, httpProxyPort);
+        } else {
+            webClient = new WebClient(BrowserVersion.getDefault());
+        }
+    }
+
+    @After
+    public void tearDown() {
+        webClient.close();
     }
 
     @Test
     public void testGET() throws Exception {
-        try (WebClient webClient = new WebClient()) {
-            Page page = webClient.getPage(baseUrl + "/test.php");
-            String content = page.getWebResponse().getContentAsString();
-            Assert.assertEquals("text/plain", page.getWebResponse().getContentType());
-            Assert.assertEquals("1", content);
-        }
+        Page page = webClient.getPage(baseUrl + "/test.php");
+        String content = page.getWebResponse().getContentAsString();
+        Assert.assertEquals("text/plain", page.getWebResponse().getContentType());
+        Assert.assertEquals("1", content);
+
 
     }
 
