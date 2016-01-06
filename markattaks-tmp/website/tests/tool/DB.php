8de02b3be9906409e9d76a3e3ab11e187a19a250
mdubois6@UNIV-FCOMTE.FR <mdubois6@UNIV-FCOMTE.FR@31180c16-7d4d-425e-b24e-9feb305d5524>
2016-01-06 20:03:59 +0000

    updated db test tool
    git-svn-id: http://m2gl.deptinfo-st.univ-fcomte.fr/svn/m2test6@80 31180c16-7d4d-425e-b24e-9feb305d5524

--- markattaks-tmp/website/tests/tool/DB.php
+++ markattaks-tmp/website/tests/tool/DB.php
@@ -8,17 +8,10 @@ use website\db\DBTrait;
 class DB
 {
     use DBTrait;
-    //private static $done = array();
 
     public static final function init($dbname='m2test6-utest')
     {
-        /*if(!isset(self::$done[$dbname])) {
-            putenv("MYSQL_DB_NAME=$dbname");
-            $sqlScript = file_get_contents('..' . DIRECTORY_SEPARATOR . 'sql' . DIRECTORY_SEPARATOR . 'db.sql');
-            $sqlScript = str_replace('m2test6', $dbname, $sqlScript);
-            self::db()->exec($sqlScript);
-            self::$done[$dbname] = true;
-        }*/
+        putenv("MYSQL_DB_NAME=$dbname");
         self::db()->beginTransaction();
     }
 
