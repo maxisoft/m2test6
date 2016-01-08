d99cfff43a11c0451205d9cb76af18228ae63cf9
mdubois6@UNIV-FCOMTE.FR <mdubois6@UNIV-FCOMTE.FR@31180c16-7d4d-425e-b24e-9feb305d5524>
2016-01-08 11:47:13 +0000

    updated bootstrap
    git-svn-id: http://m2gl.deptinfo-st.univ-fcomte.fr/svn/m2test6@90 31180c16-7d4d-425e-b24e-9feb305d5524

--- markattaks-tmp/website/db/bootstrap/index.php
+++ markattaks-tmp/website/db/bootstrap/index.php
@@ -17,8 +17,11 @@ $admin->last_name = "nameless";
 $admin->login = 'admin';
 $admin->password = $bananaHashed;
 $admin->role = 'admin';
+$admin->address = 'nowhere';
+$admin->date_of_birth = '2000-01-01';
+$admin->phone = '000';
+$admin->email = 'admin@mail.com';
 $admin->save();
-
 
 $prof = new User();
 $prof->first_name = "teneyug";
@@ -26,6 +29,10 @@ $prof->last_name = "nameless";
 $prof->login = 'teacher';
 $prof->password = $bananaHashed;
 $prof->role = 'teacher';
+$prof->address = 'nowhere';
+$prof->date_of_birth = '2000-01-01';
+$prof->phone = '000';
+$prof->email = 'teneyug@mail.com';
 $prof->save();
 
 $student = new User();
@@ -34,4 +41,8 @@ $student->last_name = "nameless";
 $student->login = 'student';
 $student->password = $bananaHashed;
 $student->role = 'student';
+$student->address = 'nowhere';
+$student->date_of_birth = '2000-01-01';
+$student->phone = '000';
+$student->email = 'franklin@mail.com';
 $student->save();
